0d1bda82bd2818ef0971ad84995777bbe65ec005
Leandro Amaral <lramaral02m@gmail.com>
2025-04-04 17:17:23 -0300

    Criando context

---
+++ APIProEventos/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using APIProEventos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIProEventos.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
